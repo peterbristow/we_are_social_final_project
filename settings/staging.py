f4d9a181c146d75709210e844d0f0adeeb6791ff
Peter <peterjb73@gmail.com>
2016-06-10 14:09:54 +0100

    Added dj_database_url

--- settings/staging.py
+++ settings/staging.py
@@ -1,4 +1,5 @@
 from base import *
+import dj_database_url
 
 DEBUG = True
 
@@ -9,11 +10,13 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.parse('mysql://b73a49feeb6804:7272322c@eu-cdbr-west-01.cleardb.com/heroku_c792b4d395a7d48?reconnect=true')
+
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_gxQDf9QL5eKhTgDpoWr560AP')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_WODOSCNhYTVcDu7nHCnnp34A')
 
 # Paypal environment variables
-SITE_URL = 'http://<your Heroku app name>.herokuapp.com'
-PAYPAL_NOTIFY_URL = '<your Heroku URL>'
+SITE_URL = 'https://wearesocialfinalproject.herokuapp.com'
+PAYPAL_NOTIFY_URL = 'https://wearesocialfinalproject.herokuapp.com/a-very-hard-to-guess-url/'
 PAYPAL_RECEIVER_EMAIL = 'peterjb73+1@gmail.com'
